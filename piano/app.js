82d6f50daf6aac8c83569fb6b84a24b1a5c148b2
Jonathan Geiger <78149229+geiger01@users.noreply.github.com>
2021-04-23 11:58:21 +0300

    Add files via upload

---
+++ piano/app.js
@@ -0,0 +1,55 @@
+const whiteKeys= ['z', 'x', 'c', 'v', 'b', 'n', 'm',"q",'w','e','r','t','y','u'];
+const blackKeys= ['s', 'd', 'g', 'h', 'j', '2','3','5','6','7'];
+
+const whiteNotes = document.querySelectorAll(".key.white");
+const blackNotes = document.querySelectorAll(".key.black");
+const keys = document.querySelectorAll(".key");
+
+document.addEventListener('keydown', e => {
+    const key = e.key;
+    // console.log(key);
+    const whiteKeyIndex = whiteKeys.indexOf(key);
+    const blackKeyIndex = blackKeys.indexOf(key);
+
+    if (whiteKeyIndex > -1){playNote(whiteNotes[whiteKeyIndex])}
+    if (blackKeyIndex > -1){playNote(blackNotes[blackKeyIndex])}
+})
+
+document.addEventListener('keyup', e => {
+    const key = e.key;
+    const whiteKeyIndex = whiteKeys.indexOf(key);
+    const blackKeyIndex = blackKeys.indexOf(key);
+
+    if (whiteKeyIndex > -1){pauseNote(whiteNotes[whiteKeyIndex])}
+    if (blackKeyIndex > -1){pauseNote(blackNotes[blackKeyIndex])}
+})
+
+
+
+keys.forEach(key => {
+    key.addEventListener("mousedown", () => playNote(key));
+    // key.addEventListener("click", () => playNote(key));
+    key.addEventListener("mouseup", () => pauseNote(key));
+
+})
+
+function playNote(key){
+    const noteAudio = document.getElementById(key.dataset.note);
+    // noteAudio.currentTime = 0;
+    noteAudio.play();
+
+    key.classList.add('active');
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+    })
+
+}
+
+function pauseNote(key){
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.currentTime = 0;
+    noteAudio.pause();
+    key.classList.remove('active');
+}
+
+
